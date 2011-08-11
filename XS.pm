0ed4d3cf21e0d866ec15feb89cd22d20fb90bba9
Marc Lehmann <schmorp@schmorp.de>
2011-08-11 17:07:26 +0000

    Import of MLEHMANN/JSON-XS-2.32 from CPAN.
    gitpan-cpan-distribution: JSON-XS
    gitpan-cpan-version:      2.32
    gitpan-cpan-path:         MLEHMANN/JSON-XS-2.32.tar.gz
    gitpan-cpan-author:       MLEHMANN
    gitpan-cpan-maturity:     released

--- XS.pm
+++ XS.pm
@@ -103,7 +103,7 @@ package JSON::XS;
 
 use common::sense;
 
-our $VERSION = '2.31';
+our $VERSION = '2.32';
 our @ISA = qw(Exporter);
 
 our @EXPORT = qw(encode_json decode_json to_json from_json);
