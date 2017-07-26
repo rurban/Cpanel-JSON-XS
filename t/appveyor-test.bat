fc998aa82c20ee3f05c5b5cf77e7b67db45e2d13
Pali <pali@cpan.org>
2017-07-26 23:58:15 +0200

    In AppVeyor batch use $Config{make} instead of dmake and propagate errors

--- t/appveyor-test.bat
+++ t/appveyor-test.bat
@@ -11,14 +11,14 @@ if "%MSVC_CPERL%" == "1" goto msvc
 :gcc
 
 set PATH=C:\strawberry\perl\bin;C:\strawberry\perl\site\bin;C:\strawberry\c\bin;%PATH%
-rem echo cpan -T %REQS%
-rem cpan -T %REQS%
+echo cpan -T %REQS%
+call cpan -T %REQS% || exit /b 1
 echo perl Makefile.PL
-perl Makefile.PL
-echo dmake
-dmake
-echo dmake test
-dmake test
+perl Makefile.PL || exit /b 1
+echo $Config{make}
+perl -MConfig -e "system({$Config{make}} $Config{make}); exit(($? < 0 || $? & 127) ? 1 : ($? >> 8));" || exit /b 1
+echo $Config{make} test
+perl -MConfig -e "system({$Config{make}} $Config{make}, 'test'); exit(($? < 0 || $? & 127) ? 1 : ($? >> 8));" || exit /b 1
 
 exit /b
 
@@ -26,11 +26,15 @@ exit /b
 if "%PLATFORM%" == "x64" set PLATFORM=amd64
 rem 14 deviates from cperl with linker errors for the libc runtime
 set MSVC_VERSION=12
-call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsall.bat" %PLATFORM%
+call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsall.bat" %PLATFORM% || exit /b 1
 
 set PATH=C:\cperl\bin;C:\cperl\site\bin;%PATH%
-cperl -S cpan -T %REQS%
-cperl Makefile.PL
-nmake
-nmake test
+echo cperl -S cpan -T %REQS%
+cperl -S cpan -T %REQS% || exit /b 1
+echo cperl Makefile.PL
+cperl Makefile.PL || exit /b 1
+echo nmake
+nmake || exit /b 1
+echo nmake test
+nmake test || exit /b 1
 
